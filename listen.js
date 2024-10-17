bb78a19a19bafaa81194eee8b2685771f963ab13
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2024-10-17 21:10:42 +0100

    completes part of ticket-14-core-host-application

--- listen.js
+++ listen.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const { PORT = 8888 } = process.env
 
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}...`);
+    console.log(`listening on ${PORT}...`)
 })
